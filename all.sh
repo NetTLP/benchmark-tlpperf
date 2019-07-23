14b7c4394667317671eed4ae2c9c9c2e5c628c1b
Ryo Nakamura <upa@haeena.net>
2019-07-23 20:12:17 +0900

    add graphs on dma write

--- all.sh
+++ all.sh
@@ -2,7 +2,10 @@
 
 
 for pattern in seq random fix; do
-	./plot-x-cpu.py -p $pattern -u bps
-	./plot-x-cpu.py -p $pattern -u tps
+#	./plot-x-cpu.py -d read -p $pattern -u bps
+#	./plot-x-cpu.py -d read -p $pattern -u tps
+	./plot-x-cpu.py -d write -p $pattern -u bps
+	./plot-x-cpu.py -d write -p $pattern -u tps
 done
+	
 	
