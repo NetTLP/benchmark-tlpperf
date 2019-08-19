59ae51d25eb5077a64d59bfe06863cd7263a5d2e
Ryo Nakamura <upa@haeena.net>
2019-08-19 23:23:54 +0900

    revert to before MRRS 2048

--- test.sh
+++ test.sh
@@ -7,16 +7,16 @@ NETTLP_NIC_ADDR=192.168.10.1
 # parameters must be changed in accordance with TLP NIC host
 # DMA_REGION_ADDR and size is decided by tlpperf -S
 NETTLP_NIC_BUS=1b:00
-DMA_REGION_ADDR=0x749000000
+DMA_REGION_ADDR=0x747400000
 DMA_REGION_SIZE=8392704
 
 # parameters for testing
 DURATION=10
 
-#directions=(read write)
-directions=(read)
+directions=(read write)
 #dma_lens=(1 4 8 16 32 64 128 256)
-dma_lens=(1024 2048)
+#dma_lens=(512)
+dma_lens=(1024)
 cpu_nums=(1 2 4 6 8 10 12 14 16)
 patterns=(fix seq random)
 
